74d49c986296151fb5043d1533906340419db8a7
S T <itschibits@gmail.com>
2021-05-17 18:59:37 -0400

    message to and from

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 "use strict";
 const bcrypt = require("bcrypt");
-const {BCRYPT_WORK_FACTOR, DB_URI} = require("../config");
+const { BCRYPT_WORK_FACTOR, DB_URI } = require("../config");
 const db = require("../db");
 const { NotFoundError } = require("../expressError");
 
@@ -28,13 +28,13 @@ class User {
     const result = await db.query(
       `SELECT password
       FROM users
-      WHERE username = $1`, 
+      WHERE username = $1`,
       [username]);
     const user = result.rows[0];
     if (user) {
       if (await bcrypt.compare(password, user.password) === true) {
         return true;
-      } 
+      }
     }
     return false;
   }
@@ -46,7 +46,7 @@ class User {
       `UPDATE users
       SET last_login_at = current_timestamp
       WHERE username = $1
-      RETURNING username`, 
+      RETURNING username`,
       [username]);
     // return result.rows[0];
     const user = result.rows[0];
@@ -59,6 +59,12 @@ class User {
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const result = await db.query(
+      `SELECT username, first_name, last_name
+      FROM users
+      ORDER BY username`
+    )
+    return result.rows
   }
 
   /** Get: get user by username
@@ -73,9 +79,9 @@ class User {
   static async get(username) {
     const result = await db.query(
       // don't need parentheses around selected fields
-      `SELECT (username, first_name, last_name, phone, join_at, last_login_at)
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at
       FROM users
-      WHERE username = $1`, 
+      WHERE username = $1`,
       [username]);
     const user = result.rows[0];
     if (!user) {
@@ -93,17 +99,76 @@ class User {
    */
 
   static async messagesFrom(username) {
-  }
+    const result = await db.query(
+      `SELECT messages.id as id,
+              messages.body as body,
+              messages.sent_at as sent_at,
+              messages.read_at as read_at,
+              messages.to_username as username,
+              users.first_name as first_name,
+              users.last_name as last_name,
+              users.phone as phone
+      FROM messages
+      JOIN users
+      ON users.username = messages.to_username
+      WHERE messages.from_username = $1`,
+      [username]
+    );
+    let messages = result.rows
+    return messages.map(m => ({
+      id: m.id,
+      to_user:
+      {
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone,
+        username: m.username
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at
+    }));
+
+  };
 
   /** Return messages to this user.
    *
    * [{id, from_user, body, sent_at, read_at}]
    *
    * where from_user is
-   *   {id, first_name, last_name, phone}
+   *   {username, first_name, last_name, phone}
    */
 
   static async messagesTo(username) {
+    const result = await db.query(
+    `SELECT messages.id as id,
+            messages.body as body,
+            messages.sent_at as sent_at,
+            messages.read_at as read_at,
+            messages.from_username as username,
+            users.first_name as first_name,
+            users.last_name as last_name,
+            users.phone as phone
+    FROM messages
+    JOIN users
+    ON users.username = messages.from_username
+    WHERE messages.to_username = $1`,
+    [username]
+  );
+    let messages = result.rows
+    return messages.map(m => ({
+      id: m.id,
+      from_user:
+      {
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone,
+        username: m.username
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at
+    }));
   }
 }
 
