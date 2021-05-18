72ea02f123b519fe2eebf1727b740cbdc7850679
Andre Pautin <andre.pautin@gmail.com>
2021-05-18 11:43:06 -0700

    add message routes and made .env file

--- routes/messages.js
+++ routes/messages.js
@@ -2,6 +2,10 @@
 
 const Router = require("express").Router;
 const router = new Router();
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+const Message = require("../models/message");
+const {UnauthorizedError} = require("../expressError");
+
 
 /** GET /:id - get detail of message.
  *
@@ -15,6 +19,16 @@ const router = new Router();
  * Makes sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+  let id = req.params.id;
+  let message = await Message.get(id);
+  if (res.locals.user.username === message.from_username || res.locals.user.username === message.to_username) {
+    return res.json({ message });
+  } else {
+    throw new UnauthorizedError();
+  }
+});
+
 
 
 /** POST / - post message.
@@ -23,6 +37,12 @@ const router = new Router();
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post("/", ensureLoggedIn, async function(req, res, next) {
+  let {to_username, body} = req.body;
+  let fromUsername = res.locals.user.username;
+  let newMessage = await Message.create({fromUsername, to_username, body});
+  return res.json({newMessage});
+})
 
 
 /** POST/:id/read - mark message as read:
@@ -32,6 +52,16 @@ const router = new Router();
  * Makes sure that the only the intended recipient can mark as read.
  *
  **/
+router.post("/:id/read", ensureCorrectUser, async function(req, res, next) {
+  let messageId = req.params.id;
+  let message = await Message.get(messageId);
+  if (res.locals.user.username === message.to_user.username) {
+    let readMessage = await Message.markRead(messageId);
+    return res.json({readMessage});
+  } else {
+    throw new UnauthorizedError();
+  }
+});
 
 
 module.exports = router;
